44bdb185034e336f912752caff3b599fd6071f2a
vxxxxc <veper1234@icloud.com>
2025-12-20 02:01:04 +0800

    try fix CI build

--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.24;
 
-import {Test, console} from "@forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {Vault} from "../src/Vault.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
