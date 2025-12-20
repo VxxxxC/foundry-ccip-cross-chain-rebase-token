02d865af048b834883697f31fc4868f29b8da3ca
vxxxxc <veper1234@icloud.com>
2025-12-20 16:56:04 +0800

    add balanceOf at interface

--- src/interfaces/IRebaseToken.sol
+++ src/interfaces/IRebaseToken.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.24;
 
 interface IRebaseToken {
-    function mint(address to, uint256 amount) external;
-    function burn(address from, uint256 amount) external;
+  function mint(address to, uint256 amount) external;
+  function burn(address from, uint256 amount) external;
+  function balanceOf(address account) external view returns (uint256);
 }
