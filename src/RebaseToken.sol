3c0181f6a318c0182fbc388dbbe616e5ee17b0cf
vxxxxc <veper1234@icloud.com>
2025-12-20 16:58:24 +0800

    fix overflow error, due to bounding range
    deposit = bound(deposit, 1e4, type(uint96).max) <- 1e4 too less , fixed
    by changed to 1e5

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.24;
 
-import {console} from "forge-std/console.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import { console } from "forge-std/console.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
  * @title RebaseToken
@@ -15,186 +15,183 @@ import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
  * @notice Each user will have their own interest rate that is global interest rate at the time of depositing
  */
 contract RebaseToken is ERC20("RebaseToken", "RBTK"), Ownable(msg.sender), AccessControl {
-    // STATE VARIABLES
-    uint256 private constant PRECISION_FACTOR = 1e18; // 1e18 = 1.0 in precision factor
-    bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
-    uint256 private s_interestRate = 5e10; // 5e10 = 0.00000005 tokens per second per token deposited
-    mapping(address => uint256) private s_userInterestRate;
-    mapping(address => uint256) private s_userLastUpdatedTimestamp;
+  // STATE VARIABLES
+  uint256 private constant PRECISION_FACTOR = 1e18; // 1e18 = 1.0 in precision factor
+  bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
+  uint256 private s_interestRate = 5e10; // 5e10 = 0.0000000005 tokens per second (about 1.5% APY)
+  mapping(address => uint256) private s_userInterestRate;
+  mapping(address => uint256) private s_userLastUpdatedTimestamp;
 
-    // ERRORS
-    error RebaseToken__InterestRateCanOnlyDecrease(uint256 currentRate, uint256 newRate);
+  // ERRORS
+  error RebaseToken__InterestRateCanOnlyDecrease(uint256 currentRate, uint256 newRate);
 
-    // EVENTS
-    event InterestRateSet(uint256 newInterestRate);
+  // EVENTS
+  event InterestRateSet(uint256 newInterestRate);
 
-    constructor() {}
+  constructor() { }
 
-    /**
-     * @notice Transfer tokens from user to other recipient
-     * @param _recipient The address of the recipient
-     * @param _amount The amount of tokens to transfer
-     * @return bool return true if the transfer was successful
-     */
-    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
-        _mintAccruedInterest(msg.sender);
-        _mintAccruedInterest(_recipient);
+  /**
+   * @notice Transfer tokens from user to other recipient
+   * @param _recipient The address of the recipient
+   * @param _amount The amount of tokens to transfer
+   * @return bool return true if the transfer was successful
+   */
+  function transfer(address _recipient, uint256 _amount) public override returns (bool) {
+    _mintAccruedInterest(msg.sender);
+    _mintAccruedInterest(_recipient);
 
-        if (_amount == type(uint256).max) {
-            _amount = balanceOf(msg.sender);
-        }
-
-        if (balanceOf(_recipient) == 0) {
-            s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
-        }
-
-        return super.transfer(_recipient, _amount);
+    if (_amount == type(uint256).max) {
+      _amount = balanceOf(msg.sender);
     }
 
-    /**
-     * @notice Transfer tokens from sender to recipient
-     * @param _sender The address of the sender
-     * @param _recipient The address of the recipient
-     * @param _amount The amount of tokens to transfer
-     * @return bool return true if the transfer was successful
-     */
-    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
-        _mintAccruedInterest(_sender);
-        _mintAccruedInterest(_recipient);
-
-        if (_amount == type(uint256).max) {
-            _amount = balanceOf(_sender);
-        }
-
-        if (balanceOf(_recipient) == 0) {
-            s_userInterestRate[_recipient] = s_userInterestRate[_sender];
-        }
-
-        return super.transferFrom(_sender, _recipient, _amount);
+    if (balanceOf(_recipient) == 0) {
+      s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
     }
 
-    function setInterestRate(uint256 newInterestRate) external onlyOwner {
-        if (newInterestRate < s_interestRate) {
-            revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, newInterestRate);
-        }
-        s_interestRate = newInterestRate;
-        emit InterestRateSet(newInterestRate);
+    return super.transfer(_recipient, _amount);
+  }
+
+  /**
+   * @notice Transfer tokens from sender to recipient
+   * @param _sender The address of the sender
+   * @param _recipient The address of the recipient
+   * @param _amount The amount of tokens to transfer
+   * @return bool return true if the transfer was successful
+   */
+  function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+    _mintAccruedInterest(_sender);
+    _mintAccruedInterest(_recipient);
+
+    if (_amount == type(uint256).max) {
+      _amount = balanceOf(_sender);
     }
 
-    /**
-     * @notice Get the principal balance of the user. This is the number of tokens that have currently been minted to the user, not including any interest that has accrued since the last time the user interacted with the protocol.
-     * @param _user The address of the user
-     * @return The principal balance of the user
-     */
-    function principalBalanceOf(address _user) external view returns (uint256) {
-        return super.balanceOf(_user);
+    if (balanceOf(_recipient) == 0) {
+      s_userInterestRate[_recipient] = s_userInterestRate[_sender];
     }
 
-    /**
-     * @notice Mint the rebase token to user when they deposit into vault
-     * @param _to The user address to mint the rebase token to
-     * @param _amount The amount of rebase token to mint
-     */
-    function mint(address _to, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
-        _mintAccruedInterest(_to);
-        s_userInterestRate[_to] = s_interestRate;
-        _mint(_to, _amount);
+    return super.transferFrom(_sender, _recipient, _amount);
+  }
+
+  function setInterestRate(uint256 newInterestRate) external onlyOwner {
+    if (newInterestRate < s_interestRate) {
+      revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, newInterestRate);
+    }
+    s_interestRate = newInterestRate;
+    emit InterestRateSet(newInterestRate);
+  }
+
+  /**
+   * @notice Get the principal balance of the user. This is the number of tokens that have currently been minted to the user, not including any interest that has accrued since the last time the user interacted with the protocol.
+   * @param _user The address of the user
+   * @return The principal balance of the user
+   */
+  function principalBalanceOf(address _user) external view returns (uint256) {
+    return super.balanceOf(_user);
+  }
+
+  /**
+   * @notice Mint the rebase token to user when they deposit into vault
+   * @param _to The user address to mint the rebase token to
+   * @param _amount The amount of rebase token to mint
+   */
+  function mint(address _to, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
+    _mintAccruedInterest(_to);
+    s_userInterestRate[_to] = s_interestRate;
+    _mint(_to, _amount);
+  }
+
+  /**
+   * @notice Burn the user tokens when they withdraw from the vault
+   * @param _from The user address to burn the rebase token from
+   * @param _amount The amount of rebase token to burn
+   */
+  function burn(address _from, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
+    _mintAccruedInterest(_from);
+    _burn(_from, _amount);
+  }
+
+  /**
+   * @notice Calculate the balance for the user including the interest that has accumulated since last updated timestamp
+   * @notice (principal balance) + some interest that has accrued
+   * @param _user The user the calculate the balance for
+   * @return The balance of the user including the interest that has accumulated since last updated timestamp
+   */
+  function balanceOf(address _user) public view override returns (uint256) {
+    // get the user current principal balance (the number of tokens have been minted to user)
+    // multiply the principal balance by interest rate that has accumulated since last updated timestamp
+    uint256 principal = super.balanceOf(_user);
+    console.log("Principal:", principal);
+    uint256 multiplier = _calculateUserAccumulatedInterestSinceLastUpdated(_user);
+    console.log("Multiplier:", multiplier);
+
+    if (multiplier == 1) {
+      return principal;
     }
 
-    /**
-     * @notice Burn the user tokens when they withdraw from the vault
-     * @param _from The user address to burn the rebase token from
-     * @param _amount The amount of rebase token to burn
-     */
-    function burn(address _from, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
-        if (_amount == type(uint256).max) {
-            _amount = this.balanceOf(_from);
-        }
-        _mintAccruedInterest(_from);
-        _burn(_from, _amount);
-    }
+    return (principal * multiplier / PRECISION_FACTOR);
+  }
 
-    /**
-     * @notice Calculate the balance for the user including the interest that has accumulated since last updated timestamp
-     * @notice (principal balance) + some interest that has accrued
-     * @param _user The user the calculate the balance for
-     * @return The balance of the user including the interest that has accumulated since last updated timestamp
-     */
-    function balanceOf(address _user) public view override returns (uint256) {
-        // get the user current principal balance (the number of tokens have been minted to user)
-        // multiply the principal balance by interest rate that has accumulated since last updated timestamp
-        uint256 principal = super.balanceOf(_user);
-        console.log("Principal:", principal);
-        uint256 multiplier = _calculateUserAccumulatedInterestSinceLastUpdated(_user);
-        console.log("Multiplier:", multiplier);
+  /**
+   * @notice Calculate the accumulated interest for user since last updated timestamp
+   * @param _user The user to calculate the accumulated interest for
+   * @return linearInterest The accumulated interest multiplier
+   */
+  function _calculateUserAccumulatedInterestSinceLastUpdated(address _user)
+    internal
+    view
+    returns (uint256 linearInterest)
+  {
+    // we need to calculate the interest that has accumulated since the last updated
+    // this will going to be linear growth with time
+    // 1. Calculate the time since last updated
+    // 2. Calculate the amount of linear growth
+    //
+    // (principal amount) + principal amount * user interest rate * time elapsed
+    // deposit : 10 tokens
+    // interest rate: 0.5 token per seconds
+    // time elapsed: 2 seconds
+    // total = 10 + 10 * 0.5 * 2 = 20 tokens
+    uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
+    linearInterest = PRECISION_FACTOR + (s_userInterestRate[_user] * timeElapsed);
+  }
 
-        if (multiplier == 1) {
-            return principal;
-        }
+  /**
+   * @notice Mint the accrued interest to user since the last time they interacted with the protocol (e.g. mint, burn, transfer)
+   * @param _user The user to mint the accrued interest to
+   */
+  function _mintAccruedInterest(address _user) internal {
+    // [1] Find their current balance of rebase tokens that have minted to user -> principal balance
+    uint256 principalBalance = super.balanceOf(_user);
+    // [2] Calculate their current balance including interest -> balanceOf
+    uint256 currentBalanceWithInterest = balanceOf(_user);
+    // Calculate the number of tokens that need to be minted to user -> [2] - [1]
+    uint256 balanceIncrease = currentBalanceWithInterest - principalBalance;
+    // Set the user last updated timestamp
+    s_userLastUpdatedTimestamp[_user] = block.timestamp;
+    // Call _mint to mint the tokens to the user
+    _mint(_user, balanceIncrease);
+  }
 
-        return (principal * multiplier / PRECISION_FACTOR);
-    }
+  /**
+   *
+   * @notice Get the interest rate for user
+   * @param _user The address of the user
+   * @return uint256 The interest rate of the user
+   */
+  function getUserInterestRate(address _user) external view returns (uint256) {
+    return s_userInterestRate[_user];
+  }
 
-    /**
-     * @notice Calculate the accumulated interest for user since last updated timestamp
-     * @param _user The user to calculate the accumulated interest for
-     * @return linearInterest The accumulated interest multiplier
-     */
-    function _calculateUserAccumulatedInterestSinceLastUpdated(address _user)
-        internal
-        view
-        returns (uint256 linearInterest)
-    {
-        // we need to calculate the interest that has accumulated since the last updated
-        // this will going to be linear growth with time
-        // 1. Calculate the time since last updated
-        // 2. Calculate the amount of linear growth
-        //
-        // (principal amount) + principal amount * user interest rate * time elapsed
-        // deposit : 10 tokens
-        // interest rate: 0.5 token per seconds
-        // time elapsed: 2 seconds
-        // total = 10 + 10 * 0.5 * 2 = 20 tokens
-        uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
-        linearInterest = PRECISION_FACTOR + (s_userInterestRate[_user] * timeElapsed);
-    }
+  /**
+   * @notice Get the global interest rate of the contract that is currently set. Any future depositors will receive this interest rate.
+   * @return uint256 global interest rate
+   */
+  function getInterestRate() external view returns (uint256) {
+    return s_interestRate;
+  }
 
-    /**
-     * @notice Mint the accrued interest to user since the last time they interacted with the protocol (e.g. mint, burn, transfer)
-     * @param _user The user to mint the accrued interest to
-     */
-    function _mintAccruedInterest(address _user) internal {
-        // [1] Find their current balance of rebase tokens that have minted to user -> principal balance
-        uint256 principalBalance = super.balanceOf(_user);
-        // [2] Calculate their current balance including interest -> balanceOf
-        uint256 currentBalanceWithInterest = balanceOf(_user);
-        // Calculate the number of tokens that need to be minted to user -> [2] - [1]
-        uint256 balanceIncrease = currentBalanceWithInterest - principalBalance;
-        // Set the user last updated timestamp
-        s_userLastUpdatedTimestamp[_user] = block.timestamp;
-        // Call _mint to mint the tokens to the user
-        _mint(_user, balanceIncrease);
-    }
-
-    /**
-     *
-     * @notice Get the interest rate for user
-     * @param _user The address of the user
-     * @return uint256 The interest rate of the user
-     */
-    function getUserInterestRate(address _user) external view returns (uint256) {
-        return s_userInterestRate[_user];
-    }
-
-    /**
-     * @notice Get the global interest rate of the contract that is currently set. Any future depositors will receive this interest rate.
-     * @return uint256 global interest rate
-     */
-    function getInterestRate() external view returns (uint256) {
-        return s_interestRate;
-    }
-
-    function grantMintAndBurnRole(address _account) external onlyOwner {
-        _grantRole(MINT_AND_BURN_ROLE, _account);
-    }
+  function grantMintAndBurnRole(address _account) external onlyOwner {
+    _grantRole(MINT_AND_BURN_ROLE, _account);
+  }
 }
--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -2,65 +2,137 @@
 
 pragma solidity ^0.8.24;
 
-import {Test, console} from "forge-std/Test.sol";
-import {RebaseToken} from "../src/RebaseToken.sol";
-import {Vault} from "../src/Vault.sol";
-import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+import { Test } from "forge-std/Test.sol";
+import { console } from "forge-std/console.sol";
+import { RebaseToken } from "../src/RebaseToken.sol";
+import { Vault } from "../src/Vault.sol";
+import { IRebaseToken } from "../src/interfaces/IRebaseToken.sol";
 
 contract RebaseTokenTest is Test {
-    RebaseToken private rebaseToken;
-    Vault private vault;
+  RebaseToken private rebaseToken;
+  Vault private vault;
 
-    address public owner = makeAddr("owner");
-    address public user = makeAddr("user");
+  address public owner = makeAddr("owner");
+  address public user = makeAddr("user");
+  address public anotherUser = makeAddr("anotherUser");
 
-    error RebaseTokenTest__DepositFailed();
+  error RebaseTokenTest__DepositFailed();
 
-    function setUp() public {
-        vm.startPrank(owner);
-        vm.deal(owner, 1e18); // fund the owner with 1 ETH
-        rebaseToken = new RebaseToken();
-        vault = new Vault(address(IRebaseToken(address(rebaseToken))));
-        rebaseToken.grantMintAndBurnRole(address(vault));
+  function setUp() public {
+    vm.startPrank(owner);
+    vm.deal(owner, 1e18); // fund the owner with 1 ETH
+    rebaseToken = new RebaseToken();
+    vault = new Vault(address(IRebaseToken(address(rebaseToken))));
+    rebaseToken.grantMintAndBurnRole(address(vault));
 
-        (bool success,) = payable(address(vault)).call{value: 1e18}(""); // fund the vault with 1 ETH via fallback
-        if (!success) {
-            revert RebaseTokenTest__DepositFailed();
-        }
-
-        vm.stopPrank();
+    (bool success,) = payable(address(vault)).call{ value: 1e18 }(""); // fund the vault with 1 ETH via fallback
+    if (!success) {
+      revert RebaseTokenTest__DepositFailed();
     }
 
-    function testDepositLinear(uint256 amount) public {
-        vm.assume(amount > 1e4); // minimum deposit amount from fuzzy input
-        amount = bound(amount, 1e4, type(uint96).max); // bound the fuzzy input to the amount from 0.01 ETH to max uint96)
-        // 1. deposit
-        vm.startPrank(user);
-        vm.deal(user, amount); // set input amount for fuzzy testing
-        vault.deposit{value: amount}();
+    vm.stopPrank();
+  }
 
-        // 2. check out rebase token balance
-        uint256 initialBalance = rebaseToken.balanceOf(user);
-        console.log("Initial balance:", initialBalance);
-        assertEq(initialBalance, amount);
+  function addRewardsToVault(uint256 rewardAmount) public {
+    (bool success, ) = payable(address(vault)).call{value: rewardAmount}("");
+  }
 
-        // 3. warp the time and check the balance again
-        vm.warp(block.timestamp + 1 hours);
-        uint256 middleBalance = rebaseToken.balanceOf(user);
-        console.log("New balance after 1 hour:", middleBalance);
-        assertGt(middleBalance, initialBalance);
+  function testDepositLinear(uint256 amount) public {
+    vm.assume(amount > 1e4); // minimum deposit amount from fuzzy input
+    amount = bound(amount, 1e4, type(uint96).max); // bound the fuzzy input to the amount from 0.01 ETH to max uint96)
+    // 1. deposit
+    vm.startPrank(user);
+    vm.deal(user, amount); // set input amount for fuzzy testing
+    vault.deposit{ value: amount }();
 
-        // 4. warp the time again by the same amount and check the balance again
-        vm.warp(block.timestamp + 1 hours);
-        uint256 finalBalance = rebaseToken.balanceOf(user);
-        console.log("New balance after another hour:", finalBalance);
-        assertGt(finalBalance, middleBalance);
+    // 2. check out rebase token balance
+    uint256 initialBalance = rebaseToken.balanceOf(user);
+    console.log("Initial balance:", initialBalance);
+    assertEq(initialBalance, amount);
 
-        // 5. check the balances between each hour to ensure linear growth
-        uint256 interestFirstHour = middleBalance - initialBalance;
-        uint256 interestSecondHour = finalBalance - middleBalance;
-        assertApproxEqAbs(interestFirstHour, interestSecondHour, 1);
+    // 3. warp the time and check the balance again
+    vm.warp(block.timestamp + 1 hours);
+    uint256 middleBalance = rebaseToken.balanceOf(user);
+    console.log("New balance after 1 hour:", middleBalance);
+    assertGt(middleBalance, initialBalance);
 
-        vm.stopPrank();
-    }
+    // 4. warp the time again by the same amount and check the balance again
+    vm.warp(block.timestamp + 1 hours);
+    uint256 finalBalance = rebaseToken.balanceOf(user);
+    console.log("New balance after another hour:", finalBalance);
+    assertGt(finalBalance, middleBalance);
+
+    // 5. check the balances between each hour to ensure linear growth
+    uint256 interestFirstHour = middleBalance - initialBalance;
+    uint256 interestSecondHour = finalBalance - middleBalance;
+    assertApproxEqAbs(interestFirstHour, interestSecondHour, 1);
+
+    vm.stopPrank();
+  }
+
+  function testRedeemStraightAway(uint256 amount) public {
+    amount = bound(amount, 1e4, type(uint96).max);
+    // 1. deposit
+    vm.startPrank(user);
+    vm.deal(user, amount);
+    vault.deposit{ value: amount }();
+    assertEq(address(user).balance, 0);
+    assertEq(rebaseToken.balanceOf(user), amount);
+
+    // 2. redeem
+    vault.redeem(type(uint256).max);
+    assertEq(rebaseToken.balanceOf(user), 0);
+    assertEq(address(user).balance, amount);
+
+    vm.stopPrank();
+  }
+
+  function testRedeemByAmount(uint256 amount) public {
+    amount = bound(amount, 1e4, type(uint96).max);
+    // 1. deposit
+    vm.startPrank(user);
+    vm.deal(user, amount);
+    vault.deposit{ value: amount }();
+    assertEq(address(user).balance, 0);
+    assertEq(rebaseToken.balanceOf(user), amount);
+
+    // 2. redeem
+    vault.redeem(amount);
+    assertEq(rebaseToken.balanceOf(user), 0);
+    assertEq(address(user).balance, amount);
+
+    vm.stopPrank();
+  }
+
+  function testRedeemAfterTimePassed(uint256 deposit, uint256 time) public {
+    time = bound(time, 1000, type(uint96).max);
+    deposit = bound(deposit, 1e5, type(uint96).max);
+    console.log("Deposit amount:", deposit);
+    console.log("Time warp seconds:", time);
+
+    // 1. deposit
+    vm.deal(user, deposit);
+    vm.prank(user);
+    vault.deposit{ value: deposit }();
+
+    // 2. warp time
+    vm.warp(block.timestamp + time);
+    uint256 balanceAfterTime = rebaseToken.balanceOf(user);
+    console.log("Balance after time warp:", balanceAfterTime);
+
+    // 2b. add rewards to the vault to simulate interest
+    vm.deal(owner, balanceAfterTime - deposit);
+    vm.prank(owner);
+    addRewardsToVault( balanceAfterTime - deposit );
+
+    // 3. redeem
+    vm.prank(user);
+    vault.redeem(type(uint256).max);
+
+    uint256 ethBalance = address(user).balance;
+    console.log("ETH balance after redeem:", ethBalance);
+
+    assertEq(ethBalance, balanceAfterTime);
+    assertGt(ethBalance, deposit);
+  }
 }
