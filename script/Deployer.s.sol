9f4d0c076db8c14124ba6937ecd14fb2e8a7882d
vxxxxc <veper1234@icloud.com>
2025-12-28 19:30:05 +0800

    [Deploy] create deploy contract
    create Token and Pool Deployer contract

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -5,16 +5,39 @@ pragma solidity ^0.8.24;
 import { Script } from "forge-std/Script.sol";
 import { Vault } from "../src/Vault.sol";
 import { IRebaseToken } from "../src/interfaces/IRebaseToken.sol";
+import { RebaseToken } from "../src/RebaseToken.sol";
+import { RebaseTokenPool } from "../src/RebaseTokenPool.sol";
+import { IERC20 } from "@openzeppelin/contracts@4.8.3/token/ERC20/IERC20.sol";
+import { CCIPLocalSimulatorFork, Register } from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import { RegistryModuleOwnerCustom } from "@chainlink-ccip/contracts/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+import { TokenAdminRegistry } from "@chainlink-ccip/contracts/tokenAdminRegistry/TokenAdminRegistry.sol";
 
-contract TokenAndPoolDeployer is Script { }
+contract TokenAndPoolDeployer is Script {
+  function run() public returns (RebaseToken rebaseToken, RebaseTokenPool rebaseTokenPool) {
+    CCIPLocalSimulatorFork ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+    Register.NetworkDetails memory networkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+    vm.startBroadcast();
+    rebaseToken = new RebaseToken();
+    rebaseTokenPool = new RebaseTokenPool(
+      IERC20(address(rebaseToken)), new address[](0), networkDetails.rmnProxyAddress, networkDetails.routerAddress
+    );
+    rebaseToken.grantMintAndBurnRole(address(rebaseTokenPool));
+    RegistryModuleOwnerCustom(networkDetails.registryModuleOwnerCustomAddress)
+      .registerAdminViaOwner(address(rebaseToken));
+    TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).acceptAdminRole(address(rebaseToken));
+    TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).setPool(address(rebaseToken), address(rebaseTokenPool));
+
+    vm.stopBroadcast();
+  }
+}
 
 contract VaultDeployer is Script {
   function run(
-    address _rebaseToken
+    address rebaseToken
   ) public returns (Vault vault) {
     vm.startBroadcast();
-    vault = new Vault(IRebaseToken(_rebaseToken));
-    IRebaseToken(_rebaseToken).grantMintAndBurnRole(address(vault));
+    vault = new Vault(IRebaseToken(rebaseToken));
+    IRebaseToken(rebaseToken).grantMintAndBurnRole(address(vault));
     vm.stopBroadcast();
   }
 }
